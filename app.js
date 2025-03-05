712a1e8769c79d83bb7381f6b63298ebd04153f0
Muhammad Naeem <65711376+mnaiem262@users.noreply.github.com>
2025-03-05 16:46:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Example: app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello from AWS EC2 acction github test!');
