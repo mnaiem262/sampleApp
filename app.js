51b2db8dfae04c8ca0112aba74b37068430f455a
Muhammad Naeem <65711376+mnaiem262@users.noreply.github.com>
2025-03-05 07:53:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS EC2 acction github test!');
+  res.send('Hello from AWS EC2 acction github test hey!');
 });
 
 app.listen(port, () => {
